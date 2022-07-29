be5aeb21c3bf9906bb9ae1fee4109fca884a2314
vargalu <vargaluka76@gmail.com>
2022-07-29 12:42:34 +0200

    fixed routing problems

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "../src/components/pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Services from "./components/pages/Services";
 import Products from "./components/pages/Products";
 import SignUp from "./components/pages/SignUp";
@@ -10,15 +10,15 @@ import SignUp from "./components/pages/SignUp";
 function App() {
   return (
     <>
-      <Router>
+      <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" exact element={Home} />
-          <Route path="/services" exact element={Services} />
-          <Route path="/products" exact element={Products} />
-          <Route path="/sign-up" exact element={SignUp} />
+          <Route path="/" exact element={<Home />} />
+          <Route path="/services" exact element={<Services />} />
+          <Route path="/products" exact element={<Products />} />
+          <Route path="/sign-up" exact element={<SignUp />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
